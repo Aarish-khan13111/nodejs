191cd03d75b689a263a6fa0f7e1b7d9f6468eeb7
aarish <aarishkhan13111@gmail.com>
2023-02-13 17:40:05 +0530

    store imgs and files and store them in cloudnry

--- imagesandforms/index.js
+++ imagesandforms/index.js
@@ -1,15 +1,71 @@
+require("dotenv").config();
 const express = require("express");
-
+const fileUpload = require("express-fileupload");
+const cloudinary = require("cloudinary").v2;
 app = express();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
+});
+
 app.set("view engine", "ejs");
 
 //middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+  })
+);
 
-app.get("/", (req, res) => {
+app.get("/myget", (req, res) => {
   console.log(req.body);
   res.send(req.body);
+});
+
+app.post("/mypost", async (req, res) => {
+  console.log(req.body);
+  console.log(req.files);
+
+  let result;
+  let imageArray = [];
+
+  //## use case for single image
+  // let file = req.files.samplefile;
+  // result = await cloudinary.uploader.upload(file.tempFilePath, {
+  //   folder: "users",
+  // });
+
+  console.log(result);
+
+  if (req.files) {
+    for (let index = 0; index < req.files.samplefile.length; index++) {
+      const result = await cloudinary.uploader.upload(
+        req.files.samplefile[index].tempFilePath,
+        {
+          folder: "users",
+        }
+      );
+
+      imageArray.push({
+        public_id: result.public_id,
+        secure_url: result.secure_url,
+      });
+    }
+  }
+
+  details = {
+    firstname: req.body.firstname,
+    lastname: req.body.lastname,
+    result,
+    imageArray,
+  };
+  console.log(details);
+  res.send(details);
 });
 app.get("/getform", (req, res) => {
   res.render("getforms");
